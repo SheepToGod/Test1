fb5b9d008d72c0f6ce095ac2cd05f79483e2b8d4
Lee Seung Han <Lee Seung Han@59.9.227.35>
2017-12-09 18:15:40 +0900

    my world 메시지 추가

--- src/firsttest/Firstmeettest.java
+++ src/firsttest/Firstmeettest.java
@@ -5,6 +5,6 @@ public class Firstmeettest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hellow world");
+		System.out.println("my world");
 	}
-
 }
